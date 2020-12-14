225ac668f8f609ae4b016cfce3eecc66193aaa78
pauladarias <pauladarias@gmail.com>
2020-12-14 12:41:09 +0000

    Add get request

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # users_API
+
+CORS OSX:
+
+open -n -a /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --args --user-data-dir="/tmp/chrome_dev_test" --disable-web-security
--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 function fetchData() {
-  console.log("hola")
+  fetch("http://35.178.207.61:8080/pubmate/api/0.1/user/1")
+  .then(response => {
+    console.log(response)
+    if (!response.ok) {
+      throw Error("ERROR")
+    }
+    return response.json()
+  }).then(data => {
+    console.log(data.username)
+    // mapping over (if array of users) each user 
+    //const html = data.users.map(user =>{
+      // return `<p>Name: ${user.name}</p>
+    //})
+    document.querySelector("#app").innerHTML = 
+      `
+      <h1>USERS</h1>
+      <div class="user">
+        <h1>${data.id}</h1>
+        <p>User Name: ${data.username}</p>
+        <p>User Email: ${data.email}</p>
+      </div>
+      `
+      
+  }).catch(err =>{
+    console.log(err)
+  })
 }
 
 fetchData();
